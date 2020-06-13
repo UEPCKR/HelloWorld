fec209823b92cbb8ce7ee62d6f55858e83e78c45
Administrator <Administrator@CN-20200306EMYG>
2020-06-13 09:52:06 +0800

    test1

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -2,6 +2,6 @@ package helloworld;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World ÕâÊÇÎÒ¸ÄµÄ¹þ¹þ¹þ");
+		System.out.println("Hello World è¿™æ˜¯æˆ‘æ”¹çš„");
 	}
 }
